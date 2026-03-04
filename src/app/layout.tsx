024e3c75b0cedbeafb32bbf07b737fe0ecb912b2
Christina Kiesz <ckiesz@gmail.com>
2026-03-03 19:02:45 -0800

    enable analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import Link from "next/link";
 import "./globals.css";
 import { Navigation } from '@/components/Navigation';
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
@@ -30,8 +31,9 @@ export default function RootLayout({
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 			>
-				      <Navigation />
+				<Navigation />
 				{children}
+				<Analytics />
 			</body>
 		</html>
 	);
